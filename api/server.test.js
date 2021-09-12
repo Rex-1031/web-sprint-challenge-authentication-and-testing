558b7d9388fde77e7cf28e221f20ba5dcb206c0a
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-09-12 11:20:52 -0400

    server.test.js complete

--- api/server.test.js
+++ api/server.test.js
@@ -1,17 +1,37 @@
-// Write your tests here
-test('sanity', () => {
+const request = require('supertest')
+const server = require('./server.js')
+const db = require('../data/dbConfig')
+
+describe('intial tests', ()=>{
+test('sanity check', () => {
   expect(true).toBe(true)
 })
 
-const request = require('supertest')
-const server = require('./server.js')
+test('environment check', () => {
+  expect(process.env.NODE_ENV).toBe('testing')})
+})
 
-describe('server.js', ()=>{
-  describe('index route', ()=>{
-    test('should return an OK status code from the index route', async ()=>{
-      const expectedStatusCode = 200
-      const response = await request(server).get('/')
-      expect(response.status).toEqual(expectedStatusCode)
+describe('Endpoint Tests', ()=>{
+  describe('[POST] register', ()=>{
+    test('responds with error when no username is entered', async ()=>{
+      const res = await request(server).post('/api/auth/register').send({ username: '', password: 12345})
+      expect(res.body).toMatchObject({message: 'username and password required'})
+    })
+    test('responds with error when no password is entered', async ()=>{
+      const res = await request(server).post('/api/auth/register').send({ username: 'Rex', password: ''})
+      expect(res.body).toMatchObject({message: 'username and password required'})
+    })
+
+    describe('[POST] login',()=>{
+      test('responds with error when no username is entered', async ()=>{
+        const res = await request(server).post('/api/auth/login').send({ username: '', password: 12345})
+        expect(res.body).toMatchObject({message: 'username and password required'})
+      })
+      test('responds with error when no password is entered', async ()=>{
+        const res = await request(server).post('/api/auth/login').send({ username: 'Rex', password: ''})
+        expect(res.body).toMatchObject({message: 'username and password required'})
+      })
     })
   })
 })
+
